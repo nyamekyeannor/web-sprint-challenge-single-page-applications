1a92ad2640b0ebe26e3df6a090a19a40da1b021e
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2021-11-12 08:28:42 -0600

    name input DONE

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const Form = () => {
-  return <h1>Form</h1>;
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`);
+  };
+  return (
+    <form id="pizza-form" onSubmit={handleSubmit}>
+      <label>
+        Enter your name:
+        <input
+          id="name-input"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Form;
